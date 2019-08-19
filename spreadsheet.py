025e93afdd4138e2b2423c0d48f6c66506fadcc1
Patrik Olin <enmejl@gmail.com>
2019-08-19 13:00:14 +0200

    started creating proper methods

--- spreadsheet.py
+++ spreadsheet.py
@@ -2,7 +2,6 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pprint
 
-
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
@@ -15,8 +14,15 @@ sheet = client.open('pynotes_notes').sheet1
 
 pp = pprint.PrettyPrinter()
 
-new_row = ['6', 'Testar att addera rad']
-sheet.append_row(new_row)
 
-all_notes = sheet.col_values(2)
-pp.pprint(all_notes)
+id = 0
+
+
+def add_new_note(note):
+    new_row = [id, note]
+    sheet.append_row(new_row)
+
+
+def get_all_notes():
+    all_notes = sheet.col_values(2)
+    pp.pprint(all_notes)
